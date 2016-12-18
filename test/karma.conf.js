867dfec3bac42757a1d8871934e5c321aafe93f2
KUO <drjkuo@gmail.com>
2016-12-17 17:59:23 -0800

    star

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-input-stars-directive/angular-input-stars.js',
+      'bower_components/angular-input-stars/angular-input-stars.js',
       'bower_components/angular-md5/angular-md5.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
