08cdb969ae41c3e793163bb3de58c9d2e14eafc3
KUO <drjkuo@gmail.com>
2016-12-17 17:20:31 -0800

    set up env

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codejobApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codejobApp
+ */
+angular.module('codejobApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
